deb7caa1ce1cff4f28288fa92307c6a202208a22
qwected <stepan.chibrikov@gmail.com>
2025-02-28 13:14:48 +0400

    decorators.ts

--- decorators.ts
+++ decorators.ts
@@ -1,20 +1,22 @@
-// Декоратор для блокировки изменения прототипа класса
 export function SealedClass(constructor: Function) {
   Object.seal(constructor);
   Object.seal(constructor.prototype);
 }
 
-// Декоратор для преобразования возвращаемой строки в верхний регистр
-export function ToUpperCase(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-  const originalMethod = descriptor.value;
+export function ToUpperCase() {
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor): PropertyDescriptor {
+    const originalMethod = descriptor.value;
 
-  descriptor.value = function (...args: any[]) {
-    const result = originalMethod.apply(this, args);
-    if (typeof result === 'string') {
-      return result.toUpperCase();
-    }
-    return result;
+    descriptor.value = function (...args: any[]) {
+      const result = originalMethod.apply(this, args);
+      if (typeof result === 'string') {
+        return result.toUpperCase();
+      }
+      return result;
+    };
+
+
+    return descriptor;
   };
-
-  return descriptor;
 }
+
